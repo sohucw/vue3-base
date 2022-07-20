4bfee2b61027ee5908f533f9778892936075b815
chenjianwei01 <chenjianwei01@baidu.com>
2022-07-20 10:50:18 +0800

    feat: 修改url

--- src/utils/routerTime.ts
+++ src/utils/routerTime.ts
@@ -4,18 +4,18 @@ let startTime = Date.now();
 export const countTime = (to: any, from: any) => {
     //获取当前时间
     const currentTime = Date.now();
-    // console.log(
-    //     '用户由 ',
-    //     from.path,
-    //     ' 页面 跳转到 ',
-    //     to.path,
-    //     ' 页面，在 ',
-    //     from.path,
-    //     ' 页面停留了 ',
-    //     currentTime - startTime,
-    //     '毫秒。转换成秒数约为：',
-    //     (currentTime - startTime) / 1000
-    // );
+    console.log(
+        '用户由 ',
+        from.path,
+        ' 页面 跳转到 ',
+        to.path,
+        ' 页面，在 ',
+        from.path,
+        ' 页面停留了 ',
+        currentTime - startTime,
+        '毫秒。转换成秒数约为：',
+        (currentTime - startTime) / 1000
+    );
     //初始化开始时间
     startTime = Date.now();
     // console.log('======== 分割线 ========');
